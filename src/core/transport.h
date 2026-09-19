5a7131c7bab28a5c31bd8d67f1c7a70e4d523ccb
Tworets <student.artemgonchar@gmail.com>
2026-09-20 00:45:06 +0500

    feat(transport): добавить интерфейс Transport для каналов доставки

---
+++ src/core/transport.h
@@ -0,0 +1,16 @@
+#ifndef TRANSPORT_H
+#define TRANSPORT_H
+
+#include <string>
+
+class Transport {
+
+	public:
+
+		virtual ~Transport() = default;
+		virtual bool send(const std::string& packet) = 0; 
+
+};
+
+
+#endif
