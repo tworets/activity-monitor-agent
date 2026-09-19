df4c2e4d235f5cf87369fdabfab6298423a30819
Tworets <student.artemgonchar@gmail.com>
2026-09-20 00:38:19 +0500

    feat(windows): добавить функцию получения имени компьютера

---
+++ src/platform/windows/windows_system_info.cpp
@@ -0,0 +1,25 @@
+#include "windows_system_info.h"
+#include "wide_to_utf8.h"
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
+#include <string>
+
+std::string getComputerName(){
+	
+	// максимальная длина имени компьютера + ноль-терминатор
+	DWORD bufferSize { MAX_COMPUTERNAME_LENGTH + 1 };
+
+	std::wstring buffer(bufferSize, L'\0');
+
+	if (!GetComputerNameW(buffer.data(), &bufferSize)) {
+		return {};
+	}
+
+	buffer.resize(bufferSize);
+
+	return wideToUtf8(buffer);
+
+};
---
+++ src/platform/windows/windows_system_info.h
@@ -0,0 +1,8 @@
+#ifndef WINDOWS_SYSTEM_INFO_H
+#define WINDOWS_SYSTEM_INFO_H
+
+#include <string>
+
+std::string getComputerName();
+
+#endif
