0656be30d9fc50e6a18b53310fad34d3ed7d3629
Tworets <student.artemgonchar@gmail.com>
2026-09-20 21:21:55 +0500

    feat(packet): добавить билдер формирования  JSON-пакетов

---
+++ src/core/packet_builder.cpp
@@ -0,0 +1,23 @@
+#include "packet_builder.h"
+
+#include "metrics.h"
+
+#include <nlohmann/json.hpp>
+
+#include <deque>
+#include <chrono>
+#include <utility>
+
+std::string PacketBuilder::build(const std::deque<Metrics>& metrics) const {
+
+    const auto now = std::chrono::system_clock::now();
+    const auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(
+        now.time_since_epoch()).count();
+
+    nlohmann::json packet;
+    packet["agent_id"] = agentId_;
+    packet["timestamp"] = timestamp;
+    packet["payload"] = metrics;
+
+    return packet.dump();
+}
---
+++ src/core/packet_builder.h
@@ -0,0 +1,23 @@
+#ifndef PACKET_BUILDER_H
+#define PACKET_BUILDER_H
+
+#include "metrics.h"
+
+#include <deque>
+#include <string>
+#include <utility>
+
+class PacketBuilder {
+
+    public:
+
+        explicit PacketBuilder(std::string agentId) : agentId_{ std::move(agentId) } {};
+
+        std::string build(const std::deque<Metrics>& metrics) const;
+
+    private:
+
+        std::string agentId_;
+};
+
+#endif
